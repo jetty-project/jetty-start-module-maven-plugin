a7575321086a8e80a021636f14c9d8c3a341eb89
olivier lamy <olamy@apache.org>
2021-03-03 19:51:36 +1000

    fix verify script
    Signed-off-by: olivier lamy <olamy@apache.org>

--- src/it/simple-it/verify.groovy
+++ src/it/simple-it/verify.groovy
@@ -3,8 +3,9 @@ assert gcloudDatastore.isFile()
 gcloudDatastoreContent = gcloudDatastore.text
 assert gcloudDatastoreContent.contains( "maven://com.google.cloud/google-cloud-datastore/1.105.0|lib/gcloud/google-cloud-datastore-1.105.0.jar" )
 
-
-def configFile = new java.util.jar.JarFile( new File(basedir,"target/simple-it-unixsocket-mod-1.0-SNAPSHOT-config.jar"), false)
+File conf = new File(basedir,"target/simple-it-gcloud-mod-1.0-SNAPSHOT-config.jar")
+assert conf.exists()
+java.util.jar.JarFile configFile = new java.util.jar.JarFile( conf, false)
 assert configFile.getEntry('modules/gcloud/index.yaml') != null
 assert configFile.getEntry('modules/gcloud-datastore.mod') != null
 assert configFile.getEntry('modules/gcloud.mod') != null
